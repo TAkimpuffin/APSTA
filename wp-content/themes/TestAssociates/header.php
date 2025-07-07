f8d882f0f235d0c9b95fb3f3df77b782a301c7ad
TAkimpuffin <kim.puffin@testassociates.co.uk>
2025-07-07 16:22:40 +0100

    APSTA Update

---
+++ wp-content/themes/TestAssociates/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180"
+    href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+    href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/site.webmanifest">
+    <title>TITLE</title>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
+</head>
+
+<body>
+
+<div class="container container--black preheader">
+    <div class="container__inner">
+
+    </div>
+</div>
+
+<div class="container container--black header__wrapper">
+    <div class="container__inner header">
+        <div class="header__logo">
+
+        </div>
+        <div class="header__menu">
+            
+        </div>
+    </div>
+</div>
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1751985340;s:7:"authKey";s:64:"j,-)_Ma@G3k<x*ZujQFr^HzUt@`sRRo$^OK67]-3H6R<E2hOXOB&kO%q(/wNr~F)";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1464;}
