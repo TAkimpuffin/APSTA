b6f416bd9d88b2d9926c468eb314a6d4af09058d
TAkimpuffin <kim.puffin@testassociates.co.uk>
2025-08-07 12:42:14 +0100

    fixed hero mobile versions

--- wp-content/themes/TestAssociates/parts/hero.php
+++ wp-content/themes/TestAssociates/parts/hero.php
@@ -11,9 +11,15 @@ $pretty_date = formatDateWithSuffix($event_date);
     style="background: url('<?php echo $bg; ?>');">
     <div class="container__inner hero">
         <div class="hero__counter" id="event-countdown" data-date="<?php echo esc_attr($event_date); ?>">
-            <h2><i class="fa-regular fa-calendar-days"></i><?php echo $pretty_date ?> <span></span> <i
-                    class="fa-solid fa-location-dot"></i><?php the_field('he_location'); ?></h2>
+            <div class="hero__info">
+                <h2><i class="fa-regular fa-calendar-days" aria-hidden="true"></i><?php echo $pretty_date ?></h2>
+                    <span></span>
+                <h2><i class="fa-solid fa-location-dot"></i><?php the_field('he_location'); ?></h2>
+            </div>
+
             <span class="hero__timer" id="countdown-timer">Loading countdown...</span>
+
+
             <span class="hero__thankyou" id="thank-you-message" style="display: none;">Thank you for attending!</span>
         </div>
     </div>
